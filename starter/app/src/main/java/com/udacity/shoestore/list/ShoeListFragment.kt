d7cbcc6f73d70c957024059e0f1af6f7dcccccb0
Adam Giergun <adam.giergun@gmail.com>
2021-01-21 17:28:16 +0100

    Add ShoeListFragment and ShoeListViewModel

---
+++ starter/app/src/main/java/com/udacity/shoestore/list/ShoeListFragment.kt
@@ -0,0 +1,18 @@
+package com.udacity.shoestore.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.udacity.shoestore.databinding.FragmentShoeListBinding
+
+class ShoeListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return FragmentShoeListBinding.inflate(layoutInflater).root
+    }
+}
