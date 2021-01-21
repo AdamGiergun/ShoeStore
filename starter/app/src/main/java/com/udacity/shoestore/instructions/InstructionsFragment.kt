35116c3cfb50c026a04ade58a0fd27f15527e598
Adam Giergun <adam.giergun@gmail.com>
2021-01-21 12:50:54 +0100

    Add InstructionsFragment

---
+++ starter/app/src/main/java/com/udacity/shoestore/instructions/InstructionsFragment.kt
@@ -0,0 +1,18 @@
+package com.udacity.shoestore.instructions
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.udacity.shoestore.databinding.FragmentInstructionsBinding
+
+class InstructionsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return FragmentInstructionsBinding.inflate(layoutInflater).root
+    }
+}
