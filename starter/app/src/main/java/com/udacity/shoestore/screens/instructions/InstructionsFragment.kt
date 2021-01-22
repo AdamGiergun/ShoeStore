6093462966ab27f89a4b64f6b307d7febe228ab1
Adam Giergun <adam.giergun@gmail.com>
2021-01-22 11:49:24 +0100

    Add navigation from InstructionsFragment to ShoeListFragment

--- starter/app/src/main/java/com/udacity/shoestore/screens/instructions/InstructionsFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/instructions/InstructionsFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.databinding.FragmentInstructionsBinding
 
 class InstructionsFragment : Fragment() {
@@ -17,6 +18,9 @@ class InstructionsFragment : Fragment() {
         return FragmentInstructionsBinding.inflate(inflater).run{
             instructionsText1.movementMethod = ScrollingMovementMethod()
             instructionsText2.movementMethod = ScrollingMovementMethod()
+            shoeListButton.setOnClickListener {
+                findNavController().navigate(InstructionsFragmentDirections.actionInstructionsFragmentToShoeListFragment())
+            }
             root
         }
     }
